062b048c61a2bb00276825b8208aa1667b460108
mdiogc <mdiogcasko@gmail.com>
2024-04-18 15:12:29 +0100

    ets

---
+++ funciones/funciones.py
@@ -0,0 +1,4 @@
+def sum_numbers(number1, number2):
+    return number1 + number2
+
+def is_greater_than(number1, number2):
---
+++ funciones/test_funciones.py
@@ -0,0 +1,4 @@
+from funciones import sum_numbers
+
+def test_sum_numbers():
+    assert sum_numbers(2,2) == 4
